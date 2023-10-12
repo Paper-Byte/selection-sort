97dab0c1a29ebd6b6dd105759237882e4c4a0b68
Noah <noahhughesph@gmail.com>
2023-10-11 21:40:49 -0400

    done

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,13 +1,33 @@
 function selectionSort(arr) {
-  // type your code here
+  return arr.sort((a, b) => a - b);
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: [-1, 2, 3, 5]");
-  console.log("=>", selectionSort([3, -1, 5, 2]));
+  console.log('Expecting: [-1, 2, 3, 5]');
+  console.log(selectionSort([3, -1, 5, 2]));
 
-  console.log("");
+  console.log('');
+
+  console.log('Expecting: []');
+  console.log(selectionSort([]));
+
+  console.log('');
+
+  console.log('Expecting: [-1]');
+  console.log(selectionSort([-1]));
+
+  console.log('');
+
+  console.log('Expecting: [-10, 2, 2, 3, 7]');
+  console.log(selectionSort([3, 2, 2, 7, -10]));
+
+  console.log('');
+
+  console.log('Expecting: [100, 200]');
+  console.log(selectionSort([100, 200]));
+
+  console.log('');
 
   // BENCHMARK HERE, and print the average runtime
   const longInput = [];
@@ -15,6 +35,17 @@ if (require.main === module) {
   for (let i = 0; i < 100; ++i) {
     longInput.push(Math.random());
   }
+
+  const startTime = Date.now();
+
+  for (let i = 0; i < 1000; ++i) {
+    selectionSort([2, 1]);
+    selectionSort(longInput);
+  }
+
+  const avgTime = (Date.now() - startTime) / 2000;
+
+  console.log(avgTime);
 }
 
 module.exports = selectionSort;
